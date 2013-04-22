95f05cabffe1f984ed6d59f9354740d4c12c191d
Cecilia <cecilia.lejeu@gmail.com>
2013-04-22 20:05:08 +0200

    Debugging files MapManager, TowerManager and Tower

--- src/GameEngine/MapManager.java
+++ src/GameEngine/MapManager.java
@@ -1,15 +1,45 @@
+package GameEngine;
+/**
+ * Project - TowerDefense</br>
+ * <b>Class - MapManager</b></br>
+ * <p>The MapManager is the responsible for the three maps the game need and that are calculated
+ * after the original image :
+ * <ul><li> the HeightMap : that separates plains and hills</li>
+ * <li>the TerritoryMap : that generated the allowed areas of each players following the Bases
+ * they possess</li>
+ * <li> the ProximityMap : that calculate the distance of a Base and all the other Bases in
+ * game</li></ul>
+ * The MapManager create these maps into int tables before the game starts and has the 
+ * responsibility to update them when it's necessary.</br>
+ * <b>Creation :</b> 22/04/2013</br>
+ * @author K. Akyurek, A. Beauprez, T. Demenat, C. Lejeune - <b>IMAC</b></br>
+ * @see Base
+ * @see Player
+ */
+
 public class MapManager {
-
-  public <?> heightMap;
-
-  public <?> territoryMap;
-
-  public <?> proximityMap;
-
-    
-  public void initBases() {
+  /**
+   * Store the map that describes the plains and hills of the map
+   */
+  public int[] heightMap;
+  /**
+   * Store the map that determine player's territory with their possessions
+   */
+  public int[] territoryMap;
+  /**
+   * Store the map that calculate the distance between each Base
+   */
+  public int[] proximityMap;
+  /**
+   * Constructor of the MapManager class
+   */
+  public MapManager(){
+	  
   }
-
+  /**
+   * Function called each new game at the very beginning of the game, and that calculate
+   * all the maps we need.
+   */
   public void generateMaps() {
   }
 
--- src/GameEngine/TowerManager.java
+++ src/GameEngine/TowerManager.java
@@ -1,32 +1,47 @@
-import java.util.Vector;
+package GameEngine;
+import java.util.concurrent.ConcurrentHashMap;
+/**
+ * Project - TowerDefense</br>
+ * <b>Class - TowerManager</b></br>
+ * <p>The TowerManager is the class responsible for the Towers management in positionning
+ * and in leading attacks.</br>
+ * It has access to the Player's Towers and know the Unit's positions.</br>
+ * It is also responsible for the creation of a new Tower according to the territoryMap,
+ * and for the evolution of the Towers.
+ * </p> 
+ * <b>Creation :</b> 22/04/2013</br>
+ * @author K. Akyurek, A. Beauprez, T. Demenat, C. Lejeune - <b>IMAC</b></br>
+ * @see WarManager
+ * @see Tower
+ * 
+ */
 
 public class TowerManager {
-
-  public <Tower, Player> whomTowerList;
-
-    /**
-   * 
-   * @element-type Missile
+  /**
+   * Associate all the Towers in game to the Players that owns them in thread safe Map. 
    */
-  public Vector  commande;
-    /**
-   * 
-   * @element-type Tower
+  public ConcurrentHashMap<Tower, Player> whomTowerList;
+  /**
+   * Constructor of the TowerManager class
    */
-  public Vector  contrôle;
-    public WarManager utilise;
-    public Vector  myMapManager;
-  
+  public TowerManager() {
+  }
+  /**
+   * Set the selected tower active because of the proximity of a enemy Unit.
+   */
   public void activeTower() {
   }
-
+  /**
+   * Set the selected Tower inactive because no Unit's in the neighborhood.
+   */
   public void desactiveTower() {
   }
-
+  /**
+   * Create a Tower
+   */
   public void createTower() {
   }
 
-  public void <<create>>TowerManager() {
-  }
+
 
 }
