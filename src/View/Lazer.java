63e65b3e4b90f3e062fa5dd11f2bf6b17b3d6b91
Aurelie <aurelie.beauprez@gmail.com>
2013-05-21 20:44:32 +0200

    View - ArrayList conccurent exception bug fixed

---
+++ src/View/Lazer.java
@@ -0,0 +1,50 @@
+/**
+ * Project : TowerDefense
+ * By Aurélie Beauprez, Thomas Demenat, Keven Akyurek and Cecilia Lejeune
+ * Copyright IMAC 2013 - All Rights Reserved
+ *
+ * File created on 21 mai 2013
+ */
+package View;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import GameEngine.Player.PlayerType;
+
+/**
+ * @author aurelie
+ *
+ */
+public class Lazer {
+	protected int id;
+	protected Point position;
+	protected PlayerType playerType;
+	protected Point towerPosition;
+	/**
+	 * 
+	 */
+	public Lazer(int id, Point position, PlayerType playerType, Point towerPosition) {
+		super();
+		this.id = id;
+		this.position = new Point(position);
+		this.playerType = playerType;
+		this.towerPosition = new Point(towerPosition);
+	}
+	public Point getPosition() {
+		return position;
+	}
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+	public int getId() {
+		return id;
+	}
+	public PlayerType getPlayerType() {
+		return playerType;
+	}
+	public Point getTowerPosition() {
+		return towerPosition;
+	}	
+
+}
